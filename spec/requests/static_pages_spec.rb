8a3b3c374e5158269831e6bd5d7da3250e867c67
zhanpengliang <707658818@qq.com>
2015-05-06 15:01:42 +0800

    bbb

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,21 @@
+#require 'rails_helper'
+
+#RSpec.describe "StaticPages", type: :request do
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
+#      get static_pages_index_path
+#      expect(response).to have_http_status(200)
+#    end
+#  end
+#end
+#
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
